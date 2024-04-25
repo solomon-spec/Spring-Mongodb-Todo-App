cbac036c57477b8c13dc957d5389176276e2ce48
solomon Abate <solomonabate18@gmail.com>
2024-04-25 12:36:12 +0300

    Initial commit

---
+++ src/test/java/org/example/springswaggermongodbtodoapp/SpringSwaggerMongodpTodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springswaggermongodbtodoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSwaggerMongodpTodoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
