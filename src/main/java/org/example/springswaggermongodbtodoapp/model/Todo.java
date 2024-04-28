5dc582e214d95d413594b2f27c0e8ae0e75a36e8
solomon Abate <solomonabate18@gmail.com>
2024-04-28 10:44:47 +0300

    Lombok update

--- src/main/java/org/example/springswaggermongodbtodoapp/model/Todo.java
+++ src/main/java/org/example/springswaggermongodbtodoapp/model/Todo.java
@@ -1,84 +1,18 @@
 package org.example.springswaggermongodbtodoapp.model;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Todo {
 
-
     private String id;
-    @NotBlank(message = "Title cannot be blank")
     private String title;
-    @NotBlank(message = "Description cannot be blank")
     private String description;
-    @NotBlank(message = "Completed cannot be blank")
     private boolean completed;
-
-
-    public Todo(String id, String title, String description, boolean completed) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        this.completed = completed;
-
-    }
-    public Todo() {
-    }
-
-    @Override
-    public int hashCode() {
-        return super.hashCode();
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        return super.equals(obj);
-    }
-
-    @Override
-    protected Object clone() throws CloneNotSupportedException {
-        return super.clone();
-    }
-
-    @Override
-    public String toString() {
-        return "Todo{" +
-                "id='" + id + '\'' +
-                ", title='" + title + '\'' +
-                ", description='" + description + '\'' +
-                ", completed=" + completed +
-                '}';
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public boolean isCompleted() {
-        return completed;
-    }
-
-    public void setCompleted(boolean completed) {
-        this.completed = completed;
-    }
 
 }
