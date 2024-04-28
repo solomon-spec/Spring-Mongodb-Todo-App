65877fd984d804733770019a72a466a82a24c12a
solomon Abate <solomonabate18@gmail.com>
2024-04-28 10:45:10 +0300

    Lombok update

--- src/main/java/org/example/springswaggermongodbtodoapp/model/Todo.java
+++ src/main/java/org/example/springswaggermongodbtodoapp/model/Todo.java
@@ -1,6 +1,5 @@
 package org.example.springswaggermongodbtodoapp.model;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +8,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Todo {
-
     private String id;
     private String title;
     private String description;
