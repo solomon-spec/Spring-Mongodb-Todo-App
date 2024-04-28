c82bb4aa12606dbe63355955954ca7120e64ab07
solomon Abate <solomonabate18@gmail.com>
2024-04-28 14:51:44 +0300

    update todo model

--- src/main/java/org/example/springswaggermongodbtodoapp/model/Todo.java
+++ src/main/java/org/example/springswaggermongodbtodoapp/model/Todo.java
@@ -3,11 +3,15 @@ package org.example.springswaggermongodbtodoapp.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Document(collection = "todos")
 public class Todo {
+    @Id
     private String id;
     private String title;
     private String description;
