7f7372a756cedeea9bc5bdcc17938125faf0765e
solomon Abate <solomonabate18@gmail.com>
2024-04-28 14:52:46 +0300

    update rest controller to ues mongo db

--- src/main/java/org/example/springswaggermongodbtodoapp/controller/TodoController.java
+++ src/main/java/org/example/springswaggermongodbtodoapp/controller/TodoController.java
@@ -1,56 +1,70 @@
 package org.example.springswaggermongodbtodoapp.controller;
 
-import org.example.springswaggermongodbtodoapp.SpringSwaggerMongodbTodoAppApplication;
 import org.example.springswaggermongodbtodoapp.model.Todo;
+import org.example.springswaggermongodbtodoapp.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class TodoController {
+    private final TodoService todoService;
+    @Autowired
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
 
     @GetMapping("/all")
-    List<Todo> getAllTodos(){
-        return SpringSwaggerMongodbTodoAppApplication.todos;
+    ResponseEntity<List<Todo>> getAllTodos(){
+
+        return ResponseEntity.ok(todoService.findAll());
     }
 
     @PostMapping("/add")
-    public void addTodo(@RequestBody Todo todo){
-        SpringSwaggerMongodbTodoAppApplication.todos.add(todo);
+    public ResponseEntity<Todo> addTodo(@RequestBody Todo todo){
+        todo.setId(null);
+        return ResponseEntity.ok(todoService.save(todo));
     }
 
     @GetMapping("/incomplete")
-    public List<Todo> getIncompleteTodos(){
-        return SpringSwaggerMongodbTodoAppApplication.todos.stream().filter(todo -> !todo.isCompleted()).toList();
+    public ResponseEntity<List<Todo>> getIncompleteTodos(){
+        return ResponseEntity.ok(todoService.findTodosByCompleted(false));
     }
 
     @GetMapping("/complete")
-    public List<Todo> getCompleteTodos(){
-        return SpringSwaggerMongodbTodoAppApplication.todos.stream().filter(Todo::isCompleted).toList();
+    public ResponseEntity<List<Todo>> getCompleteTodos(){
+        return ResponseEntity.ok(todoService.findTodosByCompleted(true));
     }
 
     @GetMapping("/todo/{id}")
-    public Todo getTodoById(@PathVariable String id){
-        return SpringSwaggerMongodbTodoAppApplication.todos.stream().filter(todo -> todo.getId().equals(id)).findFirst().orElse(null);
+    public ResponseEntity<Todo> getTodoById(@PathVariable String id){
+        return todoService.findById(id).
+                map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 
     @PutMapping("/update/{id}")
-    public void updateTodoById(@PathVariable String id, @RequestBody Todo todo){
-        SpringSwaggerMongodbTodoAppApplication.todos.stream().filter(t -> t.getId().equals(id)).forEach(t -> {
-            t.setTitle(todo.getTitle());
-            t.setDescription(todo.getDescription());
-            t.setCompleted(todo.isCompleted());
-        });
+    public ResponseEntity<Todo> updateTodoById(@PathVariable String id, @RequestBody Todo todo){
+         return todoService.findById(id).map(existingTodo -> {
+             existingTodo.setTitle(todo.getTitle());
+             existingTodo.setDescription(todo.getDescription());
+             existingTodo.setCompleted(todo.isCompleted());
+             return ResponseEntity.ok(todoService.save(existingTodo));
+        }).orElse(ResponseEntity.notFound().build());
     }
 
     @DeleteMapping("/delete/{id}")
-    public void deleteTodoById(@PathVariable String id){
-        SpringSwaggerMongodbTodoAppApplication.todos.removeIf(todo -> todo.getId().equals(id));
+    public ResponseEntity<Void> deleteTodoById(@PathVariable String id){
+        todoService.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping("/delete/all")
-    public void deleteAllTodos(){
-        SpringSwaggerMongodbTodoAppApplication.todos.clear();
+    public ResponseEntity<Void> deleteAllTodos(){
+        todoService.deleteAll();
+        return ResponseEntity.noContent().build();
     }
 
 }
