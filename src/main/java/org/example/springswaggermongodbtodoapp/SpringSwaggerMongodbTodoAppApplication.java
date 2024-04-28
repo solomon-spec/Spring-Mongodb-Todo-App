d1b7ad4ee01f713b677bdae8eefb6ddd278716c5
solomon Abate <solomonabate18@gmail.com>
2024-04-28 14:52:23 +0300

    remove local db to change it with mongo db

--- src/main/java/org/example/springswaggermongodbtodoapp/SpringSwaggerMongodbTodoAppApplication.java
+++ src/main/java/org/example/springswaggermongodbtodoapp/SpringSwaggerMongodbTodoAppApplication.java
@@ -9,16 +9,8 @@ import java.util.List;
 
 @SpringBootApplication
 public class SpringSwaggerMongodbTodoAppApplication {
-    public static List<Todo> todos;
     public static void main(String[] args) {
-        todos = new ArrayList<Todo>();
         SpringApplication.run(SpringSwaggerMongodbTodoAppApplication.class, args);
-
-        todos.add(new Todo("1", "First Todo", "This is the first todo", false));
-        todos.add(new Todo("2", "Second Todo", "This is the second todo", false));
-        todos.add(new Todo("3", "Third Todo", "This is the third todo", false));
-        todos.add(new Todo("4", "Fourth Todo", "This is the fourth todo", true));
-
     }
 
 }
